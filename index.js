4e52e47869a59bca9299cb4d40bce6f4cd74d85d
phanTranhung <17it013@sict.udn.vn>
2020-07-21 12:18:06 +0700

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { KnexAdapter: Adapter } = require('@keystonejs/adapter-knex');
+
+const PROJECT_NAME = 'Bookstore';
+const adapterConfig = { knexOptions: { connection: 'postgres://localhost/bookstore' } };
+
+
+/**
+ * You've got a new KeystoneJS Project! Things you might want to do next:
+ * - Add adapter config options (See: https://keystonejs.com/keystonejs/adapter-mongoose/)
+ * - Select configure access control and authentication (See: https://keystonejs.com/api/access-control)
+ */
+
+const keystone = new Keystone({
+  adapter: new Adapter(adapterConfig),
+});
+
+module.exports = {
+  keystone,
+  apps: [new GraphQLApp(), new AdminUIApp({ name: PROJECT_NAME, enableDefaultRoute: true })],
+};
